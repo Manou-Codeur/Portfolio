0cd025012ea0e6c9f45530117793babe1f5e4965
khaled-codeur <manou-codeur@outlook.com>
2021-07-17 20:39:23 +0100

    added project story to 'Fash'

--- src/components/home/project/project.jsx
+++ src/components/home/project/project.jsx
@@ -14,26 +14,27 @@ const Project = ({
   }, []);
 
   return (
-    <div
-      className="project"
-      style={{ backgroundColor: backColor, color: color }}
-    >
-      <span onClick={() => isOpen(false)} style={{ borderColor: color }}>
-        Close
-      </span>
-      <h1>{projectName}</h1>
-      <h2>{projectType}</h2>
-      <img
-        src={`./imgs/${projectType}.jpg`}
-        alt="Project preview"
-        className="project__image"
-      />
-      <button style={{ border: `3px solid ${color}`, color: color }}>
-        <a href={url} target="_blank" rel="noreferrer">
-          LAUNCH SITE
-        </a>
-      </button>
-
+    <div className="project">
+      <div
+        className="project__top-part"
+        style={{ backgroundColor: backColor, color: color }}
+      >
+        <span onClick={() => isOpen(false)} style={{ borderColor: color }}>
+          Close
+        </span>
+        <h1>{projectName}</h1>
+        <h2>{projectType}</h2>
+        <img
+          src={`./imgs/${projectType}.jpg`}
+          alt="Project preview"
+          className="project__image"
+        />
+        <button style={{ border: `3px solid ${color}`, color: color }}>
+          <a href={url} target="_blank" rel="noreferrer">
+            LAUNCH SITE
+          </a>
+        </button>
+      </div>
       <ProjectStory projectName={projectName} />
     </div>
   );
--- src/components/home/project/projectStory.jsx
+++ src/components/home/project/projectStory.jsx
@@ -1,51 +1,74 @@
 const ProjectStory = ({ projectName }) => {
   if (projectName === "Fash")
     return (
-      <p className="project__story">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
-        necessitatibus numquam alias eveniet voluptatem consequuntur quidem
-        repellat illo iusto rem recusandae nobis pariatur dicta, excepturi odio
-        perferendis consectetur! Molestias eveniet repellat dolorem fugiat
-        cumque unde, voluptatum nesciunt at quae ipsum. Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Esse necessitatibus numquam alias
-        eveniet voluptatem consequuntur quidem repellat illo iusto rem
-        recusandae nobis pariatur dicta, excepturi odio perferendis consectetur!
-        Molestias eveniet repellat dolorem fugiat cumque unde, voluptatum
-        nesciunt at quae ipsum.
-      </p>
+      <div className="project__story">
+        <h2>About The Project</h2>
+        <p>
+          Fash is an e-commerce web application where you can search for some
+          products (clothes and shoes) and simulate an online payment. By the
+          Home page you will get some suggestions and trendings, there's genre
+          pages (Men, Women and kids products), you can sign in into your
+          account or create a new one, and of course it contains also a Profile
+          and Card pages.
+        </p>
+
+        <h2>Technical Sheet</h2>
+        <p>
+          Fash is a full stack web app. I did develop the front-end and Firebase
+          took care of the back-end, so i used React as UI library, Redux for
+          the state management and Formik as a form library to handle
+          validation. I worked with Jwt(json web token) to secure the user
+          authentication. By the back-end, i made a fake data base in form of a
+          big JS object that contains all the products, and another file from
+          where i can import all the db functions to get that fake data.
+        </p>
+
+        <h2>Technologies</h2>
+        <p>HTML</p>
+        <p>SCSS</p>
+        <p>JavaScript</p>
+        <p>React</p>
+        <p>React Router Dom</p>
+        <p>Redux</p>
+        <p>Material UI</p>
+        <p>Formik</p>
+        <p>Framer Motion</p>
+        <p>Firebasex</p>
+      </div>
     );
   else if (projectName === "MYSPASH")
     return (
-      <p className="project__story">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
-        necessitatibus numquam alias eveniet voluptatem consequuntur quidem
-        repellat illo iusto rem recusandae nobis pariatur dicta, excepturi odio
-        perferendis consectetur! Molestias eveniet repellat dolorem fugiat
-        cumque unde, voluptatum nesciunt at quae ipsum. Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Esse necessitatibus numquam alias
-        eveniet voluptatem consequuntur quidem repellat illo iusto rem
-        recusandae nobis pariatur dicta, excepturi odio perferendis consectetur!
-        Molestias eveniet repellat dolorem fugiat cumque unde, voluptatum
-        nesciunt at quae ipsum.
-      </p>
+      <div className="project__story">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+          necessitatibus numquam alias eveniet voluptatem consequuntur quidem
+          repellat illo iusto rem recusandae nobis pariatur dicta, excepturi
+          odio perferendis consectetur! Molestias eveniet repellat dolorem
+          fugiat cumque unde, voluptatum nesciunt at quae ipsum. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Esse necessitatibus
+          numquam alias eveniet voluptatem consequuntur quidem repellat illo
+          iusto rem recusandae nobis pariatur dicta, excepturi odio perferendis
+          consectetur! Molestias eveniet repellat dolorem fugiat cumque unde,
+          voluptatum nesciunt at quae ipsum.
+        </p>
+      </div>
     );
   else
     return (
-      <p className="project__story">
-        In my spare time, i read books about self development, i learn
-        photography and i watch scientific documentary. I contribute to AIESEC
-        projects (AIESEC is an international youth-run organization that
-        provides young people with leadership development, cross-cultural
-        internships, and global volunteer exchange experiences), thing that
-        helps me to develop my soft skills, get to know more people and grow my
-        network. <br /> <br /> In my spare time, i read books about self
-        development, i learn photography and i watch scientific documentary. I
-        contribute to AIESEC projects (AIESEC is an international youth-run
-        organization that provides young people with leadership development,
-        cross-cultural internships, and global volunteer exchange experiences),
-        thing that helps me to develop my soft skills, get to know more people
-        and grow my network.
-      </p>
+      <div className="project__story">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+          necessitatibus numquam alias eveniet voluptatem consequuntur quidem
+          repellat illo iusto rem recusandae nobis pariatur dicta, excepturi
+          odio perferendis consectetur! Molestias eveniet repellat dolorem
+          fugiat cumque unde, voluptatum nesciunt at quae ipsum. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Esse necessitatibus
+          numquam alias eveniet voluptatem consequuntur quidem repellat illo
+          iusto rem recusandae nobis pariatur dicta, excepturi odio perferendis
+          consectetur! Molestias eveniet repellat dolorem fugiat cumque unde,
+          voluptatum nesciunt at quae ipsum.
+        </p>
+      </div>
     );
 };
 
